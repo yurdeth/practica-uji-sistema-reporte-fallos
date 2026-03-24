4f001b280e11a71b77b520aba656161cb5e18bea
Angel Vasquez <angelvasquez2570@gmail.com>
2026-03-24 11:19:09 -0600

    refactor: mover scripts SQL a subdirectorio scripts

--- sql-init/main.sql
+++ sql-init/main.sql
@@ -2,14 +2,14 @@
 
 -- Ejecución manual: docker exec -it postgres17 psql -U postgres -d sistema_reporte_fallos -f /docker-entrypoint-initdb.d/main.sql
 
-\i /docker-entrypoint-initdb.d/20230323_create_tablas.sql
+\i /docker-entrypoint-initdb.d/scripts/20230323_create_tablas.sql
 
-\i /docker-entrypoint-initdb.d/20230324_create_triggers.sql
+\i /docker-entrypoint-initdb.d/scripts/20230324_create_triggers.sql
 
-\i /docker-entrypoint-initdb.d/20230324_create_alter.sql
+\i /docker-entrypoint-initdb.d/scripts/20230324_create_alter.sql
 
-\i /docker-entrypoint-initdb.d/20230324_create_indices.sql
+\i /docker-entrypoint-initdb.d/scripts/20230324_create_indices.sql
 
-\i /docker-entrypoint-initdb.d/20230324_create_script_mantenimiento.sql
+\i /docker-entrypoint-initdb.d/scripts/20230324_create_script_mantenimiento.sql
 
-\i /docker-entrypoint-initdb.d/20230324_create_metadatos.sql
+\i /docker-entrypoint-initdb.d/scripts/20230324_create_metadatos.sql
